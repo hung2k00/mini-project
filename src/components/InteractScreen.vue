481fba0d7723bbf845732890d807009163778e7a
hung2k00 <manhhungtran2k@gmail.com>
2023-05-17 14:50:21 +0700

    Update InteractScreen.vue

--- src/components/InteractScreen.vue
+++ src/components/InteractScreen.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="screen">
     <h1>This is Interact Screen</h1>
-    <card-flip />
+    <card-flip v-for="(card, index) in cardsContext" :key="index" />
   </div>
 </template>
 
 <script>
 import CardFlip from "./Card.vue";
 export default {
-  procs: {
+  props: {
     cardsContext: {
       type: Array,
       default: function () {
