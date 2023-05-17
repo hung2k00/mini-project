0d9bd9a58177bcc7f58581d51e676d32a3aecd93
hung2k00 <manhhungtran2k@gmail.com>
2023-05-17 14:34:21 +0700

    vueJS

---
+++ src/components/CopyRightScreen.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>
+    This game owned by Hung Tran in Vue 3 course-
+    <a href="#">View here</a>
+  </p>
+</template>
---
+++ src/components/InteractScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="screen">
+    <h1>This is Interact Screen</h1>
+    <card-flip />
+  </div>
+</template>
+
+<script>
+import CardFlip from "./Card.vue";
+export default {
+  procs: {
+    cardsContext: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+  components: {
+    CardFlip,
+  },
+};
+</script>
